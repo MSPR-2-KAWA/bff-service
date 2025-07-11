4d87f8c79708c881d7f89a82df049aea49d2a3d5
LoanH <lheniart@be-infotech.fr>
2025-07-11 11:01:20 +0200

    feat(order): add order

--- src/main/java/fr/epsi/service/order/OrderService.java
+++ src/main/java/fr/epsi/service/order/OrderService.java
@@ -1,8 +1,13 @@
 package fr.epsi.service.order;
 
 import fr.epsi.service.order.dto.OrderDto;
+import fr.epsi.service.order.dto.OrderProductItemDto;
+import fr.epsi.service.product.Product;
+import fr.epsi.service.product.ProductClient;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -11,6 +16,7 @@ import java.util.List;
 public class OrderService {
 
     private final OrderClient orderClient;
+    private final ProductClient productClient;
 
     public List<Order> getAll() {
         return orderClient.getAll();
@@ -25,6 +31,15 @@ public class OrderService {
     }
 
     public Order create(OrderDto createOrderDto){
+        for (OrderProductItemDto item : createOrderDto.getItems()) {
+           Product product = productClient.getById(item.getProductId());
+            if (product.getStock() < item.getQuantity()) {
+                throw new ResponseStatusException(
+                        HttpStatus.CONFLICT,
+                        "Stock insuffisant pour le produit ID : " + item.getProductId()
+                );
+            }
+        }
         return orderClient.create(createOrderDto);
     }
 
